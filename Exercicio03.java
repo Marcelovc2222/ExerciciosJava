47336d1ea47fe9d92c2fb4c59492e3790b81170e
Marcelovc2222 <marcelovsousa2222@gmail.com>
2019-09-30 23:41:07 -0300

    'InclusaoExercicio03'

---
+++ Exercicio03.java
@@ -0,0 +1,13 @@
+/**
+ * Exercicio03
+ */
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+        int soma = 0;
+        for(int x = 0 ; x <= 100 ; x+=3) {
+            System.out.println(soma);
+            soma = soma + x;
+            }
+        }
+}
